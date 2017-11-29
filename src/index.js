b34c6a05bc19cf127ce1a48cc0bd9ba21abea172
박진억 <cramis@gmail.com>
2017-11-30 00:29:04 +0900

    youtube API 구현해서 데이터 가져오기 및 videoList 간단 구현

--- src/index.js
+++ src/index.js
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
 import SearchBar from './components/search_bar';
+import YoutubeSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
 
 // Youtube API KEY
 const API_KEY = 'AIzaSyBa4ifxSToufcsBxM8ozvtOBYK3MiOmsVg';
 
-// 브라우저에 HTML을 뿌려주는 컴포넌트를 만든다.
-const App = () => {
-    return (
-    <div>
-        <SearchBar />
-    </div> 
-    ); // <-- JSX
+
+
+class App extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {videos : []};
+
+        YoutubeSearch({key : API_KEY, term : 'surfboards'}, (videos)=>{
+            this.setState({videos});
+        });
+
+    }
+
+    render(){
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div> 
+            ); 
+    }
+    
 }
 
 
 
-// 만들어진 HTML에 대한 컴포넌트를 DOM 안에 넣도록 한다.
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
