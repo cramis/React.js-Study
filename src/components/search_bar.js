305c9aa894c47a11879cf86e4c98b7c8448e2313
박진억 <cramis@gmail.com>
2017-11-29 16:09:22 +0900

    SearchBar 함수형 -> 클래스로 구현

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { Component} from 'react';
+// const Component = React.Component;
 
 
-const SearchBar = () => {
-    return <input />
+class SearchBar extends Component{
+    // render(){
+    //     return <input onChange={this.onInputChange}/>;
+    // }
+
+    // onInputChange(event){
+    //     console.log(event.target.value);
+    // }
+
+    // 위의 코드가 아래와 같이 축약될 수 있음
+    render(){
+        return <input onChange={(event) => console.log(event.target.value)}/>;
+    }
+
 }
+
+
 
 
 export default SearchBar;
