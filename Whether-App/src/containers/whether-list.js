a8afc9668bc3c4cdb24fa4f8ac1df989f45b8020
박진억 <cramis@gmail.com>
2017-12-04 16:38:11 +0900

    스타일 적용 및 구글맵 적용

--- Whether-App/src/containers/whether-list.js
+++ Whether-App/src/containers/whether-list.js
@@ -1,12 +1,26 @@
 import React, {Component} from 'react';
-
 import {connect} from 'react-redux';
+
+import Chart from '../components/chart';
+import GoogleMap from '../components/google-map';
 
 class WhetherList extends Component{
     renderWhether(info){
+        const cityName =  info.city.name;
+        const temps = info.list.map((whether) => whether.main.temp - 273.15);
+        const pressures = info.list.map((whether) => whether.main.pressure);
+        const humidities = info.list.map((whether) => whether.main.humidity);
+        const {lat, lon} = info.city.coord;
+        
+
         return (
-            <tr>
-                <td>{info.city.name}</td>
+            <tr key={cityName}>
+                <td>
+                <GoogleMap lat={lat} lon={lon} />
+                </td>
+                <td><Chart data={temps} color="red" units="도" /></td>
+                <td><Chart data={pressures} color="green" units="hPa" /></td>
+                <td><Chart data={humidities} color="blue" units="%" /></td>
             </tr>
         );
     }
