36eaa4ab2ff1ba6c90698809cb55752df509f26d
박진억 <cramis@gmail.com>
2017-12-07 01:05:04 +0900

    PostIndex 구현

--- Blog-App/src/components/post_index.js
+++ Blog-App/src/components/post_index.js
@@ -10,17 +10,43 @@ class PostIndex extends Component {
         this.props.fetchPosts();
     }
 
+    renderPosts(){
+
+        
+        return this.props.posts.map((post) => {
+            
+            return (
+                <li className="list-group-item" key={post.id}>
+                    <span className="pull-xs-right">{post.categories}</span>
+                    <strong>{post.title}</strong>
+                </li>
+            );
+        });
+    }
+
     render(){
+        if(!this.props.posts){
+            return <div>loading...</div>;
+        }
+        
         return (
             <div>
                 <div className="text-xs-right">
                     <Link to='/posts/new' className="btn btn-primary" >새로운 포스트 쓰기</Link>
                 </div>
-                Post Index
+                <ul className="list-group">
+                    {this.renderPosts()}
+                </ul>
             </div>
         );
     }
 
 }
 
-export default connect(null, {fetchPosts})(PostIndex);
+function mapStateToProps(state){
+    return {
+        posts : state.posts.all
+    };
+}
+
+export default connect(mapStateToProps, {fetchPosts})(PostIndex);
