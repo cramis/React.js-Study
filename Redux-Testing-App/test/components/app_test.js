fbfc3d2b880b45404fca2615478ad04e9573b408
박진억 <cramis@gmail.com>
2017-12-12 23:32:36 +0900

    기본 Testing - describe, it, expect 사용

--- Redux-Testing-App/test/components/app_test.js
+++ Redux-Testing-App/test/components/app_test.js
@@ -1,14 +1,17 @@
-import { renderComponent , expect } from '../test_helper';
+import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
-describe('App' , () => {
-  let component;
 
-  beforeEach(() => {
-    component = renderComponent(App);
-  });
+describe('App', ()=>{
 
-  it('renders something', () => {
-    expect(component).to.exist;
+  it('div 안에 무슨 글이 있더라?', ()=>{
+
+    const component = renderComponent(App);
+
+    expect(component).to.contain('React simple starter');
   });
+  
+  
 });
+
+
