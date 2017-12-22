a125a267916369c63991dc21f8acc6c4ed2bdbeb
박진억 <cramis@gmail.com>
2017-12-22 11:53:22 +0900

    json  데이터를 promise 이용해 보여주는 미들웨어를 구현함

--- Middleware-Tutorial-App/src/actions/index.js
+++ Middleware-Tutorial-App/src/actions/index.js
@@ -2,13 +2,13 @@ import {
     FETCH_USERS
 } from './types';
 
+import axios from 'axios';
+
 export function fetchUsers(users){
+    const request = axios.get('https://jsonplaceholder.typicode.com/users');
+    // debugger;
     return { 
         type: FETCH_USERS,
-        payload : [
-            { name : 'Jane'},
-            { name : 'Tom'},
-            { name : 'Bill'}    
-        ]
+        payload : request
     };
 }
